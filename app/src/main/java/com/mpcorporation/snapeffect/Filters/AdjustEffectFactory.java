92352ea32c65d837824c00c3bdb04bf9f39db430
michikuni <minhphuonglcby@gmail.com>
2025-06-22 18:19:11 +0700

    thêm icon cho filter trộn ảnh + filter chỉnh ảnh

--- app/src/main/java/com/mpcorporation/snapeffect/Filters/AdjustEffectFactory.java
+++ app/src/main/java/com/mpcorporation/snapeffect/Filters/AdjustEffectFactory.java
@@ -26,80 +26,80 @@ public class AdjustEffectFactory {
         adjustEffects.add(new EffectItem(
                 "Điều chỉnh độ sáng",
                 new GPUImageBrightnessFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_brightness,
                 "Độ sáng", -1f, 1f,
                 (filter, value) -> ((GPUImageBrightnessFilter) filter).setBrightness(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Điều chỉnh tương phản",
                 new GPUImageContrastFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_contrast,
                 "Tương phản", 0f, 4f,
                 (filter, value) -> ((GPUImageContrastFilter) filter).setContrast(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Điều chỉnh độ bão hòa",
                 new GPUImageSaturationFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_saturation,
                 "Bão hòa", 0f, 2f,
                 (filter, value) -> ((GPUImageSaturationFilter) filter).setSaturation(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Điều chỉnh màu Hue",
                 new GPUImageHueFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_hue,
                 "Hue", 0f, 360f,
                 (filter, value) -> ((GPUImageHueFilter) filter).setHue(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Điều chỉnh gamma",
                 new GPUImageGammaFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_gamma,
                 "Gamma", 0f, 3f,
                 (filter, value) -> ((GPUImageGammaFilter) filter).setGamma(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Điều chỉnh phơi sáng",
                 new GPUImageExposureFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_exposure,
                 "Phơi sáng", -10f, 10f,
                 (filter, value) -> ((GPUImageExposureFilter) filter).setExposure(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Cân bằng trắng",
                 new GPUImageWhiteBalanceFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_white_balance,
                 "Cân bằng trắng", 2000f, 8000f,
                 (filter, value) -> ((GPUImageWhiteBalanceFilter) filter).setTemperature(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Curve chỉnh màu",
                 new GPUImageToneCurveFilter(),
-                R.drawable.bot_nav_transform
+                R.drawable.adjust_tone_curve
         ));
         adjustEffects.add(new EffectItem(
                 "Đảo màu",
                 new GPUImageColorInvertFilter(),
-                R.drawable.bot_nav_transform
+                R.drawable.adjust_invert_colors
         ));
         adjustEffects.add(new EffectItem(
                 "Giảm màu sắc",
                 new GPUImagePosterizeFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_colors,
                 "Giảm màu", 1, 256,
                 (filter, value) -> ((GPUImagePosterizeFilter) filter).setColorLevels((int)value.floatValue())
         ));
         adjustEffects.add(new EffectItem(
                 "Màu nâu cổ điển",
                 new GPUImageSepiaToneFilter(),
-                R.drawable.bot_nav_transform,
+                R.drawable.adjust_vintage,
                 "Cường độ", 0f, 1f,
                 (filter, value) -> ((GPUImageSepiaToneFilter) filter).setIntensity(value)
         ));
         adjustEffects.add(new EffectItem(
                 "Chuyển sang trắng đen",
                 new GPUImageGrayscaleFilter(),
-                R.drawable.bot_nav_transform
+                R.drawable.adjust_gray_scale
         ));
 
         /*
--- app/src/main/java/com/mpcorporation/snapeffect/Filters/BlendEffectFactory.java
+++ app/src/main/java/com/mpcorporation/snapeffect/Filters/BlendEffectFactory.java
@@ -38,45 +38,45 @@ public class BlendEffectFactory {
         blendEffect.add(new EffectItem(
                 "Tan mờ ảnh",
                 new GPUImageDissolveBlendFilter(),
-                R.drawable.hive_24px,
+                R.drawable.blend_dissolve,
                 "Blend",
                 0f, 1f,
                 (filter, value) -> ((GPUImageDissolveBlendFilter) filter).setMix(value)));
         blendEffect.add(new EffectItem(
                 "Trộn 2 ảnh theo tỉ lệ",
                 new GPUImageMixBlendFilter("0.5"),
-                R.drawable.hive_24px,
+                R.drawable.blend_mix,
                 "Blend",
                 0f, 1f,
                 (filter, value) -> ((GPUImageMixBlendFilter) filter).setMix(value)));
         blendEffect.add(new EffectItem(
                 "Cộng pixel ảnh",
                 new GPUImageAddBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_add));
         blendEffect.add(new EffectItem(
                 "Trộn alpha ảnh",
                 new GPUImageAlphaBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_alpha));
         blendEffect.add(new EffectItem(
                 "Xóa màu nền",
                 new GPUImageChromaKeyBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_chroma_key));
         blendEffect.add(new EffectItem(
                 "Trộn màu",
                 new GPUImageColorBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_color));
         blendEffect.add(new EffectItem(
                 "Làm tối mạnh",
                 new GPUImageColorBurnBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_color_burn));
         blendEffect.add(new EffectItem(
                 "Làm sáng mạnh",
                 new GPUImageColorDodgeBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_color_dodge));
         blendEffect.add(new EffectItem(
                 "Giữ màu tối hơn",
                 new GPUImageDarkenBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_dark));
         blendEffect.add(new EffectItem(
                 "Hiệu số pixel",
                 new GPUImageDifferenceBlendFilter(),
@@ -84,63 +84,63 @@ public class BlendEffectFactory {
         blendEffect.add(new EffectItem(
                 "Chia pixel",
                 new GPUImageDivideBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_divide));
         blendEffect.add(new EffectItem(
                 "Khác biệt mềm",
                 new GPUImageExclusionBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_exclusion));
         blendEffect.add(new EffectItem(
                 "Nhấn mạnh màu",
                 new GPUImageHardLightBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_hard_line));
         blendEffect.add(new EffectItem(
                 "Trộn màu theo Hue",
                 new GPUImageHueBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_hue));
         blendEffect.add(new EffectItem(
                 "Giữ màu sáng hơn",
                 new GPUImageLightenBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_lighten));
         blendEffect.add(new EffectItem(
                 "Làm tối tuyến tính",
                 new GPUImageLinearBurnBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_blue_linear));
         blendEffect.add(new EffectItem(
                 "Trộn theo độ sáng",
                 new GPUImageLuminosityBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_luminosity));
         blendEffect.add(new EffectItem(
                 "Nhân pixel ảnh",
                 new GPUImageMultiplyBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_multiple));
         blendEffect.add(new EffectItem(
                 "Trộn bình thường",
                 new GPUImageNormalBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_normal));
         blendEffect.add(new EffectItem(
                 "Hiệu ứng overlay",
                 new GPUImageOverlayBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_overlay));
         blendEffect.add(new EffectItem(
                 "Trộng độ bão hòa",
                 new GPUImageSaturationBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_saturation));
         blendEffect.add(new EffectItem(
                 "Làm sáng ảnh",
                 new GPUImageScreenBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_screen));
         blendEffect.add(new EffectItem(
                 "Trộn nhẹ nhàng",
                 new GPUImageSoftLightBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_soft_light));
         blendEffect.add(new EffectItem(
                 "Ghi đè ảnh gốc",
                 new GPUImageSourceOverBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_source_over));
         blendEffect.add(new EffectItem(
                 "Trừ pixel",
                 new GPUImageSubtractBlendFilter(),
-                R.drawable.hive_24px));
+                R.drawable.blend_subtract));
         return blendEffect;
     }
 }
