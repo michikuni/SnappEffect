33e4e32a422c2bbfde118f2c3e8cc6fa81178191
ducnguyen-source <minhphuongcy6kma1@gmail.com>
2025-07-03 15:46:03 +0700

    Chỉnh sửa icon

--- app/src/main/java/com/mpcorporation/snapeffect/Filters/DistortEffectFactory.java
+++ app/src/main/java/com/mpcorporation/snapeffect/Filters/DistortEffectFactory.java
@@ -17,28 +17,28 @@ public class DistortEffectFactory {
         distorEffect.add(new EffectItem(
                 "Khối pixel",
                 new GPUImagePixelationFilter(),
-                R.drawable.blur_pixel,
+                R.drawable.distort_pixel,
                 "Pixel", 1f, 100f, 7f,
                 (filter, value) -> ((GPUImagePixelationFilter) filter).setPixel(value)
         ));
         distorEffect.add(new EffectItem(
                 "Làm mờ",
                 new GPUImageGaussianBlurFilter(),
-                R.drawable.blur_gaussian,
+                R.drawable.distort_gaussian,
                 "Độ mờ", 0f, 10f, 0f,
                 (filter, value) -> ((GPUImageGaussianBlurFilter) filter).setBlurSize(value)
         ));
         distorEffect.add(new EffectItem(
                 "Xoáy hình ảnh",
                 new GPUImageSwirlFilter(),
-                R.drawable.distor_swirl,
+                R.drawable.distort_swirl,
                 "Độ xoáy", 0f, 2f, 0.1f,
                 (filter, value) -> ((GPUImageSwirlFilter) filter).setAngle(value)
         ));
         distorEffect.add(new EffectItem(
                 "Mờ zoom trung tâm",
                 new GPUImageZoomBlurFilter(),
-                R.drawable.distor_zoom_blur,
+                R.drawable.distort_zoom_blur,
                 "Kích thước mờ", 0f, 2f, 2f,
                 (filter, value) -> ((GPUImageZoomBlurFilter) filter).setBlurSize(value)
         ));
