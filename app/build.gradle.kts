3ca3987de555aaaf0287fbf9e45dfdf5247f20ce
dungtran123-cyber <dangminhphuong20042003@gmail.com>
2025-08-15 14:48:56 +0700

    release update path 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mpcorporation.snapeffect"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 12
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
